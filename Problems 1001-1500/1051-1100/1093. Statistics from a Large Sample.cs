aa855c152bf130f41615f053657a27c21e062ac4
lipeng Ma <malp0403@gmail.com>
2023-11-11 16:50:06 -0500

    11

---
+++ Problems 1001-1500/1051-1100/1093. Statistics from a Large Sample.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region Examples
+/*
+ You are given a large sample of integers in the range [0, 255]. Since the sample is so large, it is represented by an array count where count[k] is the number of times that k appears in the sample.
+
+Calculate the following statistics:
+
+minimum: The minimum element in the sample.
+maximum: The maximum element in the sample.
+mean: The average of the sample, calculated as the total sum of all elements divided by the total number of elements.
+median:
+If the sample has an odd number of elements, then the median is the middle element once the sample is sorted.
+If the sample has an even number of elements, then the median is the average of the two middle elements once the sample is sorted.
+mode: The number that appears the most in the sample. It is guaranteed to be unique.
+Return the statistics of the sample as an array of floating-point numbers [minimum, maximum, mean, median, mode]. Answers within 10-5 of the actual answer will be accepted.
+
+ 
+
+Example 1:
+
+Input: count = [0,1,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,3.00000,2.37500,2.50000,3.00000]
+Explanation: The sample represented by count is [1,2,2,2,3,3,3,3].
+The minimum and maximum are 1 and 3 respectively.
+The mean is (1+2+2+2+3+3+3+3) / 8 = 19 / 8 = 2.375.
+Since the size of the sample is even, the median is the average of the two middle elements 2 and 3, which is 2.5.
+The mode is 3 as it appears the most in the sample.
+Example 2:
+
+Input: count = [0,4,3,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,4.00000,2.18182,2.00000,1.00000]
+Explanation: The sample represented by count is [1,1,1,1,2,2,2,3,3,4,4].
+The minimum and maximum are 1 and 4 respectively.
+The mean is (1+1+1+1+2+2+2+3+3+4+4) / 11 = 24 / 11 = 2.18181818... (for display purposes, the output shows the rounded number 2.18182).
+Since the size of the sample is odd, the median is the middle element 2.
+The mode is 1 as it appears the most in the sample.
+ 
+
+Constraints:
+
+count.length == 256
+0 <= count[i] <= 109
+1 <= sum(count) <= 109
+The mode of the sample that count represents is unique.
+ */
+#endregion
+
+#region Test
+/*
+ 
+            var obj = new _1093();
+            obj.SampleStats(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            obj.SampleStats(new int[] { 0, 4, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            obj.SampleStats(new int[] { 0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+
+ */
+#endregion
+
+namespace leetcode.Problems_1001_1500._1051_1100
+{
+    internal class _1093
+    {
+        #region 11/07/2023
+        public double[] SampleStats(int[] count)
+        {
+            double total = 0;
+            double min = int.MaxValue;
+            double max = int.MinValue;
+            int maxAppear = 0;
+            int mode = 0;
+            double sum = 0;
+            for(int i =0; i < count.Length; i++)
+            {
+                if (count[i] > 0)
+                {
+                    if (count[i] > maxAppear)
+                    {
+                        maxAppear = count[i];
+                        mode = i;
+                    }
+
+                    total += count[i];
+                    if(min == int.MaxValue) { min = i; }
+                    max = Math.Max(max, i);
+                    sum += (i * count[i]);
+                }
+            }
+            // 1 2 3 4 5 6 7 8 9
+            double media = 0;
+            // if total is odd
+            if (total % 2 == 1)
+            {
+                int countsoFar = 0;
+                double target = (int)(total / 2) + 1;
+
+                for (int i = 0; i < count.Length; i++)
+                {
+                    countsoFar += count[i];
+                    if (countsoFar >= target)
+                    {
+                        media = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                double target = total / 2;
+                int countsoFar = 0;
+                for(int i =0; i < count.Length; i++)
+                {
+                    countsoFar += count[i];
+                    if(countsoFar >= target + 1)
+                    {
+                        media = i;
+                        break;
+                    }
+                    else if(countsoFar == target)
+                    {
+                        media = i;
+                        for(int k=i+1;k<count.Length;k++)
+                        {
+                            if (count[k] != 0)
+                            {
+                                media += k;
+                                break;
+                            }
+                        }
+                        media /= 2;
+                        break;
+                    }
+                }
+            }
+
+
+            return new double[] { min, max, sum / (long)total, media, mode };
+        }
+        #endregion
+    }
+}
---
+++ Problems 1001-1500/1351-1400/1360. Number of Days Between Two Dates.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Examples
+/*
+ Write a program to count the number of days between two dates.
+
+The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
+
+ 
+
+Example 1:
+
+Input: date1 = "2019-06-29", date2 = "2019-06-30"
+Output: 1
+Example 2:
+
+Input: date1 = "2020-01-15", date2 = "2019-12-31"
+Output: 15
+ 
+
+Constraints:
+
+The given dates are valid dates between the years 1971 and 2100.
+ */
+#endregion
+
+#region Test
+/*
+             var obj2 = new _1360() { };
+            obj2.DaysBetweenDates("2020-01-15", "2019-12-31");
+ */
+#endregion
+
+namespace leetcode.Problems_1001_1500._1351_1400
+{
+    internal class _1360
+    {
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            string[] arr1= date1.Split('-');
+            string[] arr2= date2.Split("-");
+            List<int> l1 = new List<int>();
+            List<int> l2 = new List<int>();
+            foreach (var item in arr1)
+            {
+                l1.Add(int.Parse(item));
+            }
+            foreach (var item in arr2)
+            {
+                l2.Add(int.Parse(item));
+            }
+            if(new DateTime(l1[0], l1[1], l1[2]) >new DateTime(l2[0], l2[1], l2[2]))
+            {
+                List<int> temp = l1.Select(x=>x).ToList();
+                l1 = l2;
+                l2 = temp;
+            }
+
+            Dictionary<int,int> months = new Dictionary<int, int>();
+            months.Add(1, 31);
+            months.Add(2, 28);
+            months.Add(3, 31);
+            months.Add(4, 30);
+            months.Add(5, 31);
+            months.Add(6, 30);
+            months.Add(7, 31);
+            months.Add(8, 31);
+            months.Add(9, 30);
+            months.Add(10, 31);
+            months.Add(11, 30);
+            months.Add(12, 31);
+
+            int days = 0;
+            for(int i = l1[0];i < l2[0]; i++)
+            {
+
+                if (DateTime.IsLeapYear(i))
+                {
+                    days += 366;
+                }
+                else
+                {
+                    days += 365;
+                }
+            }
+
+            for(int i =1; i< l1[1]; i++)
+            {
+                if(i != 2)
+                {
+                    days -= months[i];
+                }
+                else
+                {
+                    if (DateTime.IsLeapYear(l1[0]))
+                    {
+                        days -= 29;
+                    }
+                    else
+                    {
+                        days -= 28;
+                    }
+                }
+            }
+            days -= l1[2];
+
+            for(int i =1; i < l2[1]; i++)
+            {
+                if (i != 2)
+                {
+                    days += months[i];
+                }
+                else
+                {
+                    if (DateTime.IsLeapYear(l2[0]))
+                    {
+                        days += 29;
+                    }
+                    else
+                    {
+                        days += 28;
+                    }
+                }
+            }
+            days += l2[2];
+
+            return days;
+
+
+
+
+
+
+
+
+        }
+    }
+}
