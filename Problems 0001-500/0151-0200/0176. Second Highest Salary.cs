0ae377474be450db0389fec14dcd7a43f098448b
lipeng Ma <malp0403@gmail.com>
2024-06-11 15:14:21 -0400

    06/11/2024

--- Problems 0001-500/0151-0200/0176. Second Highest Salary.cs
+++ Problems 0001-500/0151-0200/0176. Second Highest Salary.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace leetcode.Problems_0001_500._0151_0200
 {
-    internal class _0175
+    internal class _0176
     {
-        #region 08/14/2023
-        //select p.firstname, p.lastname, a.city, a.state
-        //    from Person p
-        //    left join Address a on p.personId  = a.personId
-        #endregion
-
         #region 05/08/2024
         /*
          
