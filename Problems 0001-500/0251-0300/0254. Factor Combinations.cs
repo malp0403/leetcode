e3b7efe8e6ac92bfedac06db514019b5e668dd5e
DELL <DELL@DESKTOP-JM8KFQI>
2024-07-09 13:58:13 -0400

    79

--- Problems 0001-500/0251-0300/0254. Factor Combinations.cs
+++ Problems 0001-500/0251-0300/0254. Factor Combinations.cs
@@ -8,18 +8,6 @@ namespace leetcode.Problems_0001_500._0251_0300
 {
     internal class _0254
     {
-        #region 07/08/2024
-        IList<IList<int>> answer = new List<IList<int>>();
-        public IList<IList<int>> GetFactors(int n)
-        {
-            return null;
-        }
-        public void backTracking(int n,List<int> curList)
-        {
-            if (n == 1) return;
-            
 
-        }
-        #endregion
     }
 }
